ddc638437f8e2a3bc44664e8fd6da335d18db1a5
s1086439 <s1086439leiden@gmail.com>
2017-05-29 17:03:23 +0200

    Opzet applicatie

---
+++ app/src/main/java/com/example/ipmedt41617/ipmedt4_h/ExerciseActivity.java
@@ -0,0 +1,13 @@
+package com.example.ipmedt41617.ipmedt4_h;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+    }
+}
