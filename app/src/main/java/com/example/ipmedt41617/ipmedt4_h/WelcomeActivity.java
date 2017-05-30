2eed94558dd5703d0a1550a94f3663bf1f1712d0
HarkMeij <mark.heij.97@gmail.com>
2017-05-30 13:25:25 +0200

    Branch Maken

--- app/src/main/java/com/example/ipmedt41617/ipmedt4_h/WelcomeActivity.java
+++ app/src/main/java/com/example/ipmedt41617/ipmedt4_h/WelcomeActivity.java
@@ -1,11 +1,11 @@
 package com.example.ipmedt41617.ipmedt4_h;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.support.v7.widget.Toolbar;
+        import android.view.MenuItem;
+        import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
